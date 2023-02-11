231e478a3423fba23436d2104a3363200be7cd3a
Tuyen Truong <tuyen.truong@gmail.com>
2023-02-10 20:00:33 -0800

    Added support for setting up Avalon environment using setup.py and conda. Install by running "python3 setup.py install"

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
-from setuptools import setup
+import os
+import sys
+
+from setuptools import Command, setup
 from setuptools.dist import Distribution
+
+
+class InstallCommand(Command):
+    """Performs a clean installation of package dependencies using Pipenv."""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('conda env remove -n avalon -y')
+        if sys.platform == 'darwin':
+            os.system('brew install coreutils')
+        os.system('conda env create -n avalon --file environment.yaml')
+        os.system('conda run -n avalon python -m avalon.install_godot_binary --overwrite')
+        os.system('conda run -n avalon python -m avalon.common.check_install')
 
 
 class BinaryDistribution(Distribution):
@@ -13,4 +35,7 @@ setup(
     # Include pre-compiled extension
     # package_data={"packagename": ["_precompiled_extension.pyd"]},
     # distclass=BinaryDistribution,
+    cmdclass={
+        'install': InstallCommand
+    }
 )
